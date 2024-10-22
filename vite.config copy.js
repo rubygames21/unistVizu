9a111fa5c266f7cd3f610641ebed10fedeb5285c
test <simonportier21@orange.fr>
2024-10-23 02:56:07 +0900

    Fix paths for GitHub Pages deployment

---
+++ vite.config copy.js
@@ -0,0 +1,3 @@
+export default {
+    base: '/unistVizu/', // Le nom de votre repo GitHub
+  };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/unistVizu/', // Remplacez par le nom de votre repo
-});
+export default {
+    base: '/unistVizu/', // Le nom de votre repo GitHub
+  };
